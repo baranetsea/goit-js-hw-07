7db6cd70226d1882322447acc7e5658e3609cb78
baranetsea <lenyaveterllc2@gmail.com>
2024-07-25 18:47:43 +0200

    add

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,40 @@
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = "";
+
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+    size += 10;
+  }
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
